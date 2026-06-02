9d0af7f1d9541fd62201364ecb22d4973d31ca51
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-02 16:30:13 -0300

    Update ex03.js

--- ex03.js
+++ ex03.js
@@ -2,7 +2,21 @@ const readline = require("readline-sync");
 
 let idades = [];
 
-for (let i = 0 ; i < 5; i++){
-    let idade = readline.question("Digite a idade da pessoa; ");
+for (let i = 0; i < 5; i++){
+    let idade = readline.question("Digite a idade: ");
     idades.push(idade);
 }
+
+let quantidadeMaiores = 0;
+let quantidadeMenores = 0;
+
+for (let i = 0; i < 5; i++){
+    if (idades[i] >= 18){
+        quantidadeMaiores++;
+    } else {
+        quantidadeMenores++;
+    }
+}
+
+console.log("Quantidade de pessoas maiores de idade:", quantidadeMaiores);
+console.log("Quantidade de pessoas menores de idade:", quantidadeMenores);
