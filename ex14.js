17432ca57b06e836500ff666c7e16ed28d427466
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-09 14:06:46 -0300

    Refactor note processing logic in ex14.js

--- ex14.js
+++ ex14.js
@@ -1,29 +1,39 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let quantidade = readline.question("Quantas notas deseja cadastrar? ");
+let quantidade = readline.question("Quantas notas deseja cadastrar? ")
 
-let notas = [];
-let soma = 0;
-let aprovados = 0;
-let reprovados = 0;
+let notas = []
 
 for (let i = 0; i < quantidade; i++){
-    let nota = readline.question("Digite a nota: ");
-    notas.push(nota);
-    soma += Number(nota);
-    if (nota >= 7){
-        aprovados++;
+    let nota = readline.question("Digite a nota: ")
+    notas.push(nota)
+}
+
+let maiorNota = notas[0]
+let menorNota = notas[0]
+let soma = 0
+let aprovados = 0
+let reprovados = 0
+
+for (let i = 0; i < quantidade; i++){
+    if (notas[i] > maiorNota){
+        maiorNota = notas[i]
+    }
+    if (notas[i] < menorNota){
+        menorNota = notas[i];
+    }
+    soma += Number(notas[i])
+    if (notas[i] >= 7){
+        aprovados++
     } else {
-        reprovados++;
+        reprovados++
     }
 }
 
-let media = soma / quantidade;
-let maiorNota = Math.max(...notas);
-let menorNota = Math.min(...notas);
+let media = soma / quantidade
 
-console.log("Maior nota:", maiorNota);
-console.log("Menor nota:", menorNota);
-console.log("Média da turma:", media);
-console.log("Quantidade de aprovados:", aprovados);
-console.log("Quantidade de reprovados:", reprovados);
+console.log("Maior nota:", maiorNota)
+console.log("Menor nota:", menorNota)
+console.log("Média da turma:", media)
+console.log("Quantidade de aprovados:", aprovados)
+console.log("Quantidade de reprovados:", reprovados)
