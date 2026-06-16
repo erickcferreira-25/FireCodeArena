599920155723ccc6938fd0ec5d4d1ca7d16984f0
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:33:21 -0300

    Update ex09.js

--- ex09.js
+++ ex09.js
@@ -1,25 +1,25 @@
-const redline = require("readline-sync");
+const redline = require("readline-sync")
 
-let nomes = [];
+let nomes = []
 
 for (let i = 0; i < 5; i++){
-    let nome = readline.question("Digite um nome: ");
-    nomes.push(nome);
+    let nome = readline.question("Digite um nome: ")
+    nomes.push(nome)
 }
 
-let nomeProcurado = readline.question("Digite o nome que deseja procurar: ");
+let nomeProcurado = readline.question("Digite o nome que deseja procurar: ")
 
-let encontrado = false;
+let encontrado = false
 
 for (let i = 0; i < 5; i++){
     if (nomes[i] === nomeProcurado){
-        encontrado = true;
-        break;
+        encontrado = true
+        break
     }
 }
 
 if (encontrado){
-    console.log("Nome encontrado!");
+    console.log("Nome encontrado!")
 } else {
-    console.log("Nome não encontrado!");
+    console.log("Nome não encontrado!")
 }
