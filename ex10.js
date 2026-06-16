aa32286ef37ceeb151b69baccd4519f56180b9f0
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:32:11 -0300

    Update ex10.js

--- ex10.js
+++ ex10.js
@@ -1,21 +1,21 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let notas = [];
-let aprovados = [];
-let reprovados = [];
+let notas = []
+let aprovados = []
+let reprovados = []
 
 for (let i = 0; i < 8; i++){
-    let nota = readline.question("Digite a nota: ");
+    let nota = readline.question("Digite a nota: ")
     notas.push(nota);
 }
 
 for (let i = 0; i < 8; i++){
     if (notas[i] >= 7){
-        aprovados.push(notas[i]);
+        aprovados.push(notas[i])
     } else {
-        reprovados.push(notas[i]);
+        reprovados.push(notas[i])
     }
 }
 
-console.log("Aprovados:", aprovados);
-console.log("Reprovados:", reprovados);
+console.log("Aprovados:", aprovados)
+console.log("Reprovados:", reprovados)
