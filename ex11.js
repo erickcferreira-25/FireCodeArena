019734e5ddcf53e73c75130eafeea5e05e87636d
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:31:13 -0300

    Update ex11.js

--- ex11.js
+++ ex11.js
@@ -1,16 +1,16 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let numeros = [];
-let dobro = [];
+let numeros = []
+let dobro = []
 
 for (let i = 0; i < 5; i++){
-    let numero = readline.question("Digite um número: ");
-    numeros.push(numero);
+    let numero = readline.question("Digite um número: ")
+    numeros.push(numero)
 }
 
 for (let i = 0; i < 5; i++){
-    dobro.push(numeros[i] * 2);
+    dobro.push(numeros[i] * 2)
 }
 
-console.log("Números:", numeros);
-console.log("Dobro:", dobro);
+console.log("Números:", numeros)
+console.log("Dobro:", dobro)
