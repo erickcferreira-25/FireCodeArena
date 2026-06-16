6036e117a1ff5e3255ba03f64d3236681dbd287d
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:46:47 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,16 +1,16 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let quantidade = readline.question("Quantos números deseja cadastrar? ");
+let quantidade = readline.question("Quantos números deseja cadastrar? ")
 
-let pares = [];
-let impares = [];
+let pares = []
+let impares = []
 
 for (let i = 0; i < quantidade; i++){
-    let numero = readline .question("digite um número:");
+    let numero = readline .question("digite um número:")
     if (numero % 2 == 0)
-        pares.push(numero);
+        pares.push(numero)
     else
-        impares.push(numero);
+        impares.push(numero)
 }
-console.log("Números pares:", pares);
-console.log("Números ímpares:", impares);
+console.log("Números pares:", pares)
+console.log("Números ímpares:", impares)
