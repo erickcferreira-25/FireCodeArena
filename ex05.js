a3b4b4955196e409a9a3fd57fdcd22a58057fe32
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:35:34 -0300

    Update ex05.js

--- ex05.js
+++ ex05.js
@@ -1,18 +1,18 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let numeros = [];
+let numeros = []
 
 for (let i = 0; i < 5; i++){
-    let numero = readline.question("Digite um número: ");
-    numeros.push(numero);
+    let numero = readline.question("Digite um número: ")
+    numeros.push(numero)
 }
 
-let menor = numeros[0];
+let menor = numeros[0]
 
 for (let i = 1; i < 5; i++){
     if (numeros[i] < menor){
-        menor = numeros[i];
+        menor = numeros[i]
     }
 }
 
-console.log("O menor número é:", menor);
+console.log("O menor número é:", menor)
