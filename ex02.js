2e1bfcb28a0bf8ef03a3310f00fb4dada7cc0ece
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:41:19 -0300

    Update ex02.js

--- ex02.js
+++ ex02.js
@@ -1,16 +1,16 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let quantidade = readline.question("Quantos números deseja cadastrar? ");
+let quantidade = readline.question("Quantos números deseja cadastrar? ")
 
-let positivos = [];
-let negativos = [];
+let positivos = []
+let negativos = []
 
 for (let i = 0; i < quantidade; i++){
-    let numero = readline.question("Digite um número: ");
+    let numero = readline.question("Digite um número: ")
     if (numero > 0)
-        positivos.push(numero);
+        positivos.push(numero)
     else if (numero < 0)
-        negativos.push(numero);
+        negativos.push(numero)
 }
-console.log("Números positivos:", positivos);
-console.log("Números negativos:", negativos);
+console.log("Números positivos:", positivos)
+console.log("Números negativos:", negativos)
