9639c95437dda47d7ab0218099028a73906c5e07
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:34:18 -0300

    Update ex07.js

--- ex07.js
+++ ex07.js
@@ -1,19 +1,19 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let numeros = [];
+let numeros = []
 
 for(let i = 0; i < 10; i++){
-    let numero = readline.question("Digite um número: ");
-    numeros.push(numero);
+    let numero = readline.question("Digite um número: ")
+    numeros.push(numero)
 }
 
-let quantidadePares = 0;
+let quantidadePares = 0
 
 for(let i = 0; i < 10; i++){
     if(numeros[i] % 2 == 0){
-        quantidadePares++;
+        quantidadePares++
     }
 }
 
-console.log("Quantidade de números pares:", quantidadePares);
+console.log("Quantidade de números pares:", quantidadePares)
 
