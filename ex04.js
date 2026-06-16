23bd7c3cce100b0dc83ca9722606ed232a2e635e
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:37:48 -0300

    Update ex04.js

--- ex04.js
+++ ex04.js
@@ -1,18 +1,18 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let numeros = [];
+let numeros = []
 
 for (let i = 0; i < 5; i++){
-    let numero = readline.question("digite um número: ");
-    numeros.push(numero);
+    let numero = readline.question("digite um número: ")
+    numeros.push(numero)
 }
 
-let maior = numeros[0];
+let maior = numeros[0]
 
 for (let i = 1; i < 5; i++){
     if (numeros[i] > maior){
-        maior = numeros[i];
+        maior = numeros[i]
     }
 }
 
-console.log("O maior número é:", maior);
+console.log("O maior número é:", maior)
