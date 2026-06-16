844bafa9916835b7f25801e04af562cb328f3b2c
erickcferreira-25 <erickcferreira25@gmail.com>
2026-06-16 13:33:57 -0300

    Update ex08.js

--- ex08.js
+++ ex08.js
@@ -1,19 +1,19 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync")
 
-let numeros = [];
+let numeros = []
 
 for(let i = 0; i < 10; i++){
-    let numero = readline.question("Digite um número:");
-    numeros.push(numero);
+    let numero = readline.question("Digite um número:")
+    numeros.push(numero)
 }
-console.log(numeros);
+console.log(numeros)
 
-let quantidadePositivos = 0;
+let quantidadePositivos = 0
 
 for(let i = 0; i < 10; i++){
     if(numeros[i] > 0){
-        quantidadePositivos++;
+        quantidadePositivos++
     }
 }
 
-console.log("Quantidade de números positivos:", quantidadePositivos);
+console.log("Quantidade de números positivos:", quantidadePositivos)
